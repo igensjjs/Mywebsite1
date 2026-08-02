d7d5f358b4c61b97e6ec5ed2643eabae3d107bda
igensjjs <igenerator61@gmail.com>
2026-08-02 22:54:39 +0300

    Create app.js

---
+++ fronted/js/app.js
@@ -0,0 +1,7 @@
+console.log("Rehobot Choir Website Loaded");
+
+
+const year = new Date().getFullYear();
+
+document.querySelector("footer p").innerHTML =
+`© ${year} Rehobot Choir. All Rights Reserved.`;
